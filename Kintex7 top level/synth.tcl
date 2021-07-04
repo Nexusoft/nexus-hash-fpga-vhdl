3494712d8a2f109305e4147539d410ea1311089a
ahatstat <ahatstat@gmail.com>
2021-07-04 00:16:41 -0400

    update build scripts

--- Kintex7 top level/synth.tcl
+++ Kintex7 top level/synth.tcl
@@ -1,12 +1,10 @@
 # build nxs_hash for kcu105
 set_param general.maxThreads 8
 
-set outputDir ../../kcu105
+set outputDir ../../K7
 file mkdir $outputDir
 
-set_part "xcku040-ffva1156-2-e"
-set_property BOARD_PART xilinx.com:kcu105:1.7 [current_project]
-
+set_part "xc7k325tfbg676-2"
 
 # read all design files
 read_vhdl ../uart/comp/uart_tx.vhd
@@ -27,22 +25,23 @@ read_vhdl -vhdl2008 ../nxs_hash/sk1024.vhd
 read_vhdl -vhdl2008 ../nxs_hash/nxs_hash.vhd
 read_vhdl -vhdl2008 ../uart_interface/uart_nexus_pkg.vhd
 read_vhdl -vhdl2008 ../uart_interface/uart_nexus_interface.vhd
-read_vhdl -vhdl2008 KCU105_nxs_hash_top.vhd
+read_vhdl -vhdl2008 K7_nxs_hash_top.vhd
 
 
-read_ip ../../kcu105_nxs_hash_half_size/kcu105_nxs_hash_half_size.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0.xci
+read_ip ../../K7/clk_wiz_0/clk_wiz_0.xci
 # read constraints
-read_xdc KCU105_nexus_hash.xdc
+read_xdc K7_nexus_hash.xdc
 
 # generate ip
 generate_target all [get_ips]
 # Synthesize Design
-synth_design -top KCU105_nxs_hash_top -part xcku040-ffva1156-2-e -directive AreaOptimized_high -shreg_min_size 6 -retiming
+synth_design -top nxs_hash_top -directive AreaOptimized_high -part xc7k325tfbg676-2 -shreg_min_size 5
+# -retiming
 
 
 read_xdc hash_constraint_1.xdc
 #optimize design
-opt_design -resynth_remap
+opt_design -directive ExploreArea
 
 write_checkpoint -force $outputDir/post_synth.dcp
 report_timing_summary -file $outputDir/post_synth_timing_summary.rpt
